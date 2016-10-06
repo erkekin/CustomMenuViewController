9042496f56ed105cfab8d5f6a4ab3c66ae0f06a4
Erk EKİN <eekin@anadolu.edu.tr>
2016-10-06 11:19:37 +0300

    some part is done

--- CustomMenuViewController/ViewController.swift
+++ CustomMenuViewController/ViewController.swift
@@ -8,23 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController , UITextViewDelegate{
-
+class ViewController: UIViewController , UITextViewDelegate, UIPopoverPresentationControllerDelegate{
+    
+    var filterDistanceViewController:UIViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        filterDistanceViewController = (self.storyboard?.instantiateViewController(withIdentifier: "popover"))!
+        
     }
-
     
-    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
     
     func textViewDidChangeSelection(_ textView: UITextView) {
         
-        
+        print("selaaam")
+        self.filterBooks(rect: textView.caretRect(for: (textView.selectedTextRange?.end)!))
     }
     
     
+    @IBAction func filterBooks(rect: CGRect)
+    {
+        
+        DispatchQueue.main.async {
+            self.filterDistanceViewController.modalPresentationStyle = UIModalPresentationStyle.popover
+            
+            self.filterDistanceViewController.preferredContentSize = CGSize(width: 300, height: 200)
+            let popoverPresentationViewController = self.filterDistanceViewController.popoverPresentationController
+            popoverPresentationViewController?.permittedArrowDirections = .up
+            popoverPresentationViewController?.delegate = self
+            
+            popoverPresentationViewController?.sourceView = self.view;
+            
+            self.filterDistanceViewController.popoverPresentationController?.sourceRect = rect
+            
+            self.present(self.filterDistanceViewController, animated: true, completion: nil)
+            
+        }
+        
+    }
     
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none;
+    }
     
     
     
@@ -32,7 +62,7 @@ class ViewController: UIViewController , UITextViewDelegate{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
